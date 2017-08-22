03419ceaee03b8fec25110b4afd6270daf5c0101
Jesse Myers <jesse.myers@globality.com>
2017-08-21 18:38:04 -0700

    Add DAG structure for object cloning

---
+++ microcosm_postgres/tests/test_cloning.py
@@ -0,0 +1,37 @@
+"""
+Test cloning.
+
+"""
+from hamcrest import assert_that, equal_to, is_, is_not, not_none
+from microcosm.api import create_object_graph
+
+from microcosm_postgres.cloning import clone
+from microcosm_postgres.context import SessionContext, transaction
+from microcosm_postgres.tests.example import Company, CompanyType
+
+
+class TestCloning(object):
+
+    def setup(self):
+        self.graph = create_object_graph(name="example", testing=True, import_name="microcosm_postgres")
+        self.company_store = self.graph.company_store
+
+        self.context = SessionContext(self.graph)
+        self.context.recreate_all()
+        self.context.open()
+
+    def teardown(self):
+        self.context.close()
+
+    def test_clone(self):
+        with transaction():
+            company = Company(
+                name="name",
+                type=CompanyType.private,
+            ).create()
+
+        with transaction():
+            copy = clone(company, dict(name="newname"))
+
+        assert_that(copy.id, is_not(equal_to(company.id)))
+        assert_that(self.company_store.retrieve(copy.id), is_(not_none()))
---
+++ microcosm_postgres/tests/test_toposort.py
@@ -0,0 +1,38 @@
+"""
+Test topological sort.
+
+"""
+from hamcrest import assert_that, contains
+
+from microcosm_postgres.dag import Edge
+from microcosm_postgres.toposort import toposorted
+
+
+class Node(object):
+    def __init__(self, id):
+        self.id = id
+
+
+def test_toposort():
+    nodes = dict(
+        one=Node(id="one"),
+        two=Node(id="two"),
+        three=Node(id="three"),
+        four=Node(id="four"),
+    )
+    edges = [
+        Edge(from_id="one", to_id="two"),
+        Edge(from_id="one", to_id="three"),
+        Edge(from_id="two", to_id="three"),
+        Edge(from_id="three", to_id="four"),
+    ]
+
+    assert_that(
+        toposorted(nodes, edges),
+        contains(
+            nodes["one"],
+            nodes["two"],
+            nodes["three"],
+            nodes["four"],
+        ),
+    )
