6eb4587a186f5fb43626e7f874970176c5c2e6de
Jamie Chang <jamie.chang@globality.com>
2023-08-18 08:45:24 +0100

    Bump the ci build to use python 3.10

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     keywords="microcosm",
     install_requires=[
         "alembic>=1.0.0",
