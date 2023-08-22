a14b2f7b7848020378ad9d90b5d66babec441873
Jamie Chang <jamie.chang@globality.com>
2023-08-22 15:40:54 +0100

    Add multi-sharded versions of health check

--- microcosm_postgres/shards.py
+++ microcosm_postgres/shards.py
@@ -1,7 +1,9 @@
 import os
 from copy import copy
 
-from . import createall, migrate
+from sqlalchemy import text
+
+from . import createall, migrate, operations
 
 
 class subgraph:
@@ -35,14 +37,16 @@ def create_shard_specific_graph(graph, shard_name):
     )
 
 
+def get_shard_names(graph):
+    selected = os.environ.get("SHARD")
+    return [selected] if selected else list(graph.shards.keys())
+
+
 def migrate_command(graph, *args):
     """
     Run migrations for all shards.
     """
-    selected = os.environ.get("SHARD")
-    shards = [selected] if selected else graph.shards.keys()
-
-    for name in shards:
+    for name in get_shard_names(graph):
         migrate.main(create_shard_specific_graph(graph, name), *args)
 
 
@@ -50,8 +54,41 @@ def createall_command(graph):
     """
     Create all databases.
     """
-    selected = os.environ.get("SHARD")
-    shards = [selected] if selected else graph.shards.keys()
-
-    for name in shards:
+    for name in get_shard_names(graph):
         createall.main(create_shard_specific_graph(graph, name))
+
+
+def recreate_all(graph):
+    """
+    Drop all databases and recreate them.
+    """
+    for name in get_shard_names(graph):
+        operations.recreate_all(create_shard_specific_graph(graph, name))
+
+
+def check_alembic(graph):
+    """
+    Drop all databases and recreate them.
+    """
+
+    results = {}
+    for name in get_shard_names(graph):
+        with graph.sessionmakers[name]() as session:
+            results[name] = session.execute(
+                text("SELECT version_num FROM alembic_version LIMIT 1;")
+            ).scalar()
+
+    return results
+
+
+def check_health(graph):
+    """
+    Drop all databases and recreate them.
+    """
+
+    results = {}
+    for name in get_shard_names(graph):
+        with graph.sessionmakers[name]() as session:
+            results[name] = session.execute(text("SELECT 1;")).scalar()
+
+    return results
