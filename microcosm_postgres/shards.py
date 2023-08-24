c5819b5a330eea9e617234fbe902541424a6fbdb
Jamie Chang <jamie.chang@globality.com>
2023-08-24 13:40:58 +0100

    Create custom session that is limited to single bind

--- microcosm_postgres/shards.py
+++ microcosm_postgres/shards.py
@@ -1,69 +1,34 @@
-import os
-from copy import copy
-
 from sqlalchemy import text
 
 from . import createall, migrate, operations
-
-
-class subgraph:
-    def __init__(self, graph, **overrides):
-        self.graph = graph
-        self.overrides = overrides
-
-    def __getattr__(self, name):
-        if name in self.overrides:
-            return self.overrides[name]
-        return getattr(self.graph, name)
-
-
-def create_shard_specific_config(graph, shard_name):
-    new_config = copy(graph.config)
-    new_config.postgres = graph.config.shards[shard_name].postgres
-    del new_config.shards
-    return new_config
-
-
-def create_shard_specific_graph(graph, shard_name):
-    """
-    Create a new graph with a specific shard.
-
-    """
-    return subgraph(
-        graph,
-        config=create_shard_specific_config(graph, shard_name),
-        postgres=graph.shards[shard_name],
-        sessionmaker=graph.sessionmakers[shard_name],
-    )
-
-
-def get_shard_names(graph):
-    selected = os.environ.get("SHARD")
-    return [selected] if selected else list(graph.shards.keys())
+from .sharded_subgraph import create_shard_specific_graph  # noqa: F401
+from .sharded_subgraph import get_shard_names, subgraphs
 
 
 def migrate_command(graph, *args):
     """
     Run migrations for all shards.
+
+    Deprecated: Use `migrate.main` instead.
     """
-    for name in get_shard_names(graph):
-        migrate.main(create_shard_specific_graph(graph, name), *args)
+    # Backwards compatibility
+    return migrate.main(graph, *args)
 
 
 def createall_command(graph):
     """
     Create all databases.
     """
-    for name in get_shard_names(graph):
-        createall.main(create_shard_specific_graph(graph, name))
+    for subgraph in subgraphs(graph):
+        createall.main(subgraph)
 
 
 def recreate_all(graph):
     """
     Drop all databases and recreate them.
     """
-    for name in get_shard_names(graph):
-        operations.recreate_all(create_shard_specific_graph(graph, name))
+    for subgraph in subgraphs(graph):
+        operations.recreate_all(subgraph)
 
 
 def check_alembic(graph):
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 
 project = "microcosm-postgres"
-version = "3.2.1"
+version = "3.3.0"
 
 setup(
     name=project,
