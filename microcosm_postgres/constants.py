971da9409ccfb0ef7ba73fa6a17d07dda94ca308
Ronan Murphy <ronan.murphy@globality.com>
2023-08-31 12:58:21 +0100

    Use different shard override name.

--- microcosm_postgres/constants.py
+++ microcosm_postgres/constants.py
@@ -1,4 +1,4 @@
 GLOBAL_SHARD_NAME = "global"
-SHARD_OVERRIDE_NAME = "SHARD_OVERRIDE"
+SHARD_OVERRIDE_NAME = "__shard_override__"
 X_REQUEST_CLIENT_HEADER = "x-request-client"
 X_REQUEST_SHARD_HEADER = "x-request-shard"
