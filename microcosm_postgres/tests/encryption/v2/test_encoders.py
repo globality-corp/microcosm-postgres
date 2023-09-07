4e9983495666c6ed018f108c28e0ce52a9fd4c3f
Jamie Chang <jamie.chang@globality.com>
2023-09-07 13:06:05 +0100

    Add encryption v2

---
+++ microcosm_postgres/tests/encryption/v2/test_encoders.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+
+import pytest
+
+from microcosm_postgres.encryption.v2 import encoders
+
+
+@pytest.mark.parametrize(
+    ("encoder", "value"),
+    [
+        (encoders.StringEncoder(), "foo"),
+        (encoders.IntEncoder(), 1),
+        (encoders.DecimalEncoder(), Decimal(1.0)),
+        (encoders.ArrayEncoder(encoders.StringEncoder()), ["foo", "bar"]),
+        (encoders.ArrayEncoder(encoders.IntEncoder()), [1, 2]),
+        (encoders.JSONEncoder(), {"foo": "bar", "something_else": []}),
+        (encoders.Nullable(encoders.StringEncoder()), "foo"),
+        (encoders.Nullable(encoders.StringEncoder()), None),
+    ],
+)
+def test_encode_decode(encoder, value):
+    assert encoder.decode(encoder.encode(value)) == value
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 
 project = "microcosm-postgres"
-version = "2.6.0"
+version = "2.7.0"
 
 setup(
     name=project,
@@ -54,6 +54,7 @@ setup(
             "sessionmaker = microcosm_postgres.factories.sessionmaker:configure_sessionmaker",
             "sessionmakers = microcosm_postgres.factories.sessionmakers:configure_sessionmakers",
             "shards = microcosm_postgres.factories.shards:configure_shards",
+            "register_flask_context = microcosm_postgres.encryption.v2.encryptors:AwsKmsEncryptor.register_flask_context [encryption]",  # noqa: E501
         ],
     },
 )
