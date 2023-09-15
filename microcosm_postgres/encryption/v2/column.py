7b443f1b305db46de5666172049dcaba4afd1350
Jamie Chang <jamie.chang@globality.com>
2023-09-15 18:03:50 +0100

    Set default based on should_encrypt

--- microcosm_postgres/encryption/v2/column.py
+++ microcosm_postgres/encryption/v2/column.py
@@ -1,11 +1,12 @@
 from typing import (
     Any,
+    Callable,
     Generic,
     TypeVar,
     overload,
 )
 
-from sqlalchemy import ColumnElement, LargeBinary
+from sqlalchemy import LargeBinary
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -19,14 +20,15 @@ T = TypeVar("T")
 NOT_SET = object()
 
 
-class encryption_property(hybrid_property[T], Generic[T]):
+class encryption(hybrid_property[T], Generic[T]):
     @overload
     def __init__(
         self,
         key: str,
-        column_type: Any,
         encryptor: Encryptor,
         encoder: Encoder[T],
+        *,
+        column_type: Any = NOT_SET,
     ):
         ...
 
@@ -34,28 +36,28 @@ class encryption_property(hybrid_property[T], Generic[T]):
     def __init__(
         self,
         key: str,
-        column_type: Any,
         encryptor: Encryptor,
         encoder: Encoder[T],
         *,
-        default: T,
+        default: T | Callable[[], T],
+        column_type: Any = NOT_SET,
     ):
         ...
 
     def __init__(
         self,
         key: str,
-        column_type: Any,
         encryptor: Encryptor,
         encoder: Encoder[T],
         *,
+        column_type: Any = NOT_SET,
         default: Any = NOT_SET,
     ):
         self.default = default
         self.key = key
         self.encryptor = encryptor
         self.encoder = encoder
-        self.column_type = column_type
+        self.column_type = encoder.sa_type if column_type is NOT_SET else column_type
 
         encrypted_field = f"{key}_encrypted"
         unencrypted_field = f"{key}_unencrypted"
@@ -85,61 +87,37 @@ class encryption_property(hybrid_property[T], Generic[T]):
 
     def encrypted(self) -> Mapped[bytes | None]:
         if self.default is NOT_SET:
-            return mapped_column(
-                self.key + "_encrypted", LargeBinary, nullable=True
-            )
+            return mapped_column(self.key + "_encrypted", LargeBinary, nullable=True)
+
         return mapped_column(
             self.key + "_encrypted",
             LargeBinary,
             nullable=True,
-            default=lambda: self.encryptor.encrypt(self.encoder.encode(self.default)),
+            default=(
+                lambda: (
+                    self.encryptor.encrypt(
+                        self.encoder.encode(
+                            self.default() if callable(self.default) else self.default
+                        )
+                    )
+                    if self.encryptor.should_encrypt()
+                    else None
+                )
+            ),
         )
 
-    def unencrypted(self, **kwargs) -> Mapped[T | None]:
+    def unencrypted(self, **kwargs: Any) -> Mapped[T | None]:
         if self.default is NOT_SET:
             return mapped_column(self.key, self.column_type, nullable=True, **kwargs)
+
         return mapped_column(
             self.key,
             self.column_type,
             nullable=True,
-            default=self.default,
+            default=lambda: (
+                None
+                if self.encryptor.should_encrypt()
+                else (self.default() if callable(self.default) else self.default)
+            ),
             **kwargs,
         )
-
-
-def encryption(
-    key: str, encryptor: Encryptor, encoder: Encoder[T]
-) -> hybrid_property[T]:
-    """
-    Switches between encrypted and plaintext values based on the client_id.
-
-    Queries on the encryption field will only be performed on the unencrypted rows.
-    """
-    encrypted_field = f"{key}_encrypted"
-    unencrypted_field = f"{key}_unencrypted"
-
-    @hybrid_property
-    def _prop(self) -> T:
-        encrypted = getattr(self, encrypted_field)
-
-        if encrypted is None:
-            return getattr(self, unencrypted_field)
-
-        return encoder.decode(encryptor.decrypt(encrypted))
-
-    @_prop.inplace.setter
-    def _prop_setter(self, value: T) -> None:
-        encrypted = encryptor.encrypt(encoder.encode(value))
-        if encrypted is None:
-            setattr(self, unencrypted_field, value)
-            setattr(self, encrypted_field, None)
-            return
-
-        setattr(self, encrypted_field, encrypted)
-        setattr(self, unencrypted_field, None)
-
-    @_prop.inplace.expression
-    def _prop_expression(cls) -> ColumnElement[T]:
-        return getattr(cls, unencrypted_field)
-
-    return _prop
