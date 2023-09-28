8bf5306be254968553baea97e8bc3de0b36e4d7c
John Peterson <75979741+JP-Globality@users.noreply.github.com>
2023-09-28 14:17:07 +0100

    Master sync to release 2.X (#150)
    * Beaconised array matching (#149)

--- microcosm_postgres/encryption/v2/encoders.py
+++ microcosm_postgres/encryption/v2/encoders.py
@@ -6,13 +6,15 @@ from typing import (
     Any,
     Callable,
     Generic,
+    Literal,
     Protocol,
     TypeVar,
     Union,
+    overload,
 )
 
 import sqlalchemy
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import ARRAY, JSONB
 from typing_extensions import TypeAlias
 
 
@@ -31,10 +33,10 @@ class Encoder(Protocol[T]):
     class DecodeException(Exception):
         status_code = 400
 
-    def encode(self, value: T) -> str:
+    def encode(self, value: T, **kwargs) -> Union[list[str], str]:
         ...
 
-    def decode(self, value: str) -> T:
+    def decode(self, value: str, **kwargs) -> T:
         ...
 
 
@@ -71,11 +73,23 @@ class StringEncoder(Encoder[Any]):
     sa_type = sqlalchemy.String
 
     @encode_exception_wrapper
-    def encode(self, value: Any) -> str:
+    def encode(self, value: Any, **kwargs) -> str:
         return str(value)
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> Any:
+    def decode(self, value: str, **kwargs) -> Any:
+        return value
+
+
+class TextEncoder(Encoder[Any]):
+    sa_type = sqlalchemy.Text
+
+    @encode_exception_wrapper
+    def encode(self, value: Any, **kwargs) -> str:
+        return str(value)
+
+    @decode_exception_wrapper
+    def decode(self, value: str, **kwargs) -> Any:
         return value
 
 
@@ -95,11 +109,11 @@ class IntEncoder(Encoder[int]):
     sa_type = sqlalchemy.Integer
 
     @encode_exception_wrapper
-    def encode(self, value: int) -> str:
+    def encode(self, value: int, **kwargs) -> str:
         return str(value)
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> int:
+    def decode(self, value: str, **kwargs) -> int:
         return int(value)
 
 
@@ -107,11 +121,11 @@ class DecimalEncoder(Encoder[Decimal]):
     sa_type = sqlalchemy.Numeric(asdecimal=True)
 
     @encode_exception_wrapper
-    def encode(self, value: Decimal) -> str:
+    def encode(self, value: Decimal, **kwargs) -> str:
         return str(value)
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> Decimal:
+    def decode(self, value: str, **kwargs) -> Decimal:
         return Decimal(value)
 
 
@@ -119,25 +133,39 @@ class DatetimeEncoder(Encoder[datetime]):
     sa_type = sqlalchemy.DateTime(timezone=True)
 
     @encode_exception_wrapper
-    def encode(self, value: datetime) -> str:
+    def encode(self, value: datetime, **kwargs) -> str:
         return value.isoformat()
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> datetime:
+    def decode(self, value: str, **kwargs) -> datetime:
         return datetime.fromisoformat(value)
 
 
 class ArrayEncoder(Encoder["list[T]"], Generic[T]):
     def __init__(self, element_encoder: Encoder[T]):
         self.element_encoder = element_encoder
-        self.sa_type = sqlalchemy.ARRAY(element_encoder.sa_type)
+        self.sa_type = ARRAY(element_encoder.sa_type)
+
+    @overload  # type: ignore[override]
+    def encode(self, value: list[T], keep_as_array: Literal[True], **kwargs) -> list[str]:
+        ...
+
+    @overload
+    def encode(self, value: list[T], keep_as_array: Literal[False], **kwargs) -> str:
+        ...
 
     @encode_exception_wrapper
-    def encode(self, value: "list[T]") -> str:
-        return json.dumps([self.element_encoder.encode(element) for element in value])
+    def encode(self, value: list[T], keep_as_array: bool = False, **kwargs) -> "list[T] | str":
+        raw = [self.element_encoder.encode(element) for element in value]
+        if keep_as_array:
+            assert isinstance(raw, list)
+            assert isinstance(raw[0], str)
+            return raw  # type: ignore[return-value]
+        else:
+            return json.dumps(raw)
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> "list[T]":
+    def decode(self, value: str, **kwargs) -> list[T]:
         return [self.element_encoder.decode(v) for v in json.loads(value)]
 
 
@@ -145,11 +173,11 @@ class JSONEncoder(Encoder[JSONType]):
     sa_type = JSONB(none_as_null=True)
 
     @encode_exception_wrapper
-    def encode(self, value: JSONType) -> str:
+    def encode(self, value: JSONType, **kwargs) -> str:
         return json.dumps(value)
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> JSONType:
+    def decode(self, value: str, **kwargs) -> JSONType:
         return json.loads(value)
 
 
@@ -160,14 +188,17 @@ class Nullable(Encoder["Union[T, None]"], Generic[T]):
         self.sa_type = inner_encoder.sa_type
 
     @encode_exception_wrapper
-    def encode(self, value: "Union[T, None]") -> str:
+    def encode(self, value: "Union[T, None]", keep_as_array: bool = False, **kwargs) -> Union[str, list[str]]:
         if value is None:
             return json.dumps(value)
 
-        return json.dumps(self.inner_encoder.encode(value))
+        if keep_as_array:
+            return self.inner_encoder.encode(value, keep_as_array=keep_as_array)
+        else:
+            return json.dumps(self.inner_encoder.encode(value))
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> "Union[T, None]":
+    def decode(self, value: str, **kwargs) -> "Union[T, None]":
         if (loaded_value := json.loads(value)) is None:
             return None
 
@@ -188,9 +219,9 @@ class EnumEncoder(Encoder[E], Generic[E]):
         self._enum = enum
 
     @encode_exception_wrapper
-    def encode(self, value: E) -> str:
+    def encode(self, value: E, **kwargs) -> str:
         return value.name
 
     @decode_exception_wrapper
-    def decode(self, value: str) -> E:
+    def decode(self, value: str, **kwargs) -> E:
         return self._enum[value]
