3bb7ea41146af1e3a6b79412155e7c2902466b19
John Peterson <75979741+JP-Globality@users.noreply.github.com>
2023-10-06 09:47:11 +0100

    Pick first beacon in default encryptor (#160)
    * implementation for beacon in default encryptor
    
    * updating test
    
    * bump version
    
    * updating version
    
    ---------
    
    Co-authored-by: Albert Sadowski <asadowski@mailbox.org>

--- microcosm_postgres/tests/encryption/test_registry.py
+++ microcosm_postgres/tests/encryption/test_registry.py
@@ -8,6 +8,7 @@ from hamcrest import assert_that, has_entries
 from microcosm.loaders import load_from_dict
 from microcosm.object_graph import create_object_graph
 
+from microcosm_postgres.encryption.constants import ENCRYPTION_V2_DEFAULT_KEY
 from microcosm_postgres.encryption.registry import parse_config
 
 
@@ -81,3 +82,27 @@ def test_default_encryptor_not_created_when_no_config_available():
 
     multi_tenant_encryptor = graph.multi_tenant_key_registry.make_encryptor(graph)
     assert len(multi_tenant_encryptor.encryptors) == 0
+
+
+def test_first_beacon_key_used_in_default_encryptor():
+    loader = load_from_dict(
+        multi_tenant_key_registry=dict(
+            context_keys=["foo", "quux"],
+            key_ids=["bar;baz", "quuz;corge"],
+            partitions=["aws", "aws-cn"],
+            account_ids=["12345;67890", "23456;78901"],
+            restricted_kms_policy=["false", "true"],
+            beacon_keys=["beacon1", "beacon2"],
+        ),
+    )
+
+    graph = create_object_graph(
+        "example",
+        testing=True,
+        loader=loader,
+        import_name="microcosm_postgres",
+    )
+
+    multi_tenant_encryptor = graph.multi_tenant_key_registry.make_encryptor(graph)
+    default_encryptor = multi_tenant_encryptor.encryptors[ENCRYPTION_V2_DEFAULT_KEY]
+    assert default_encryptor._beacon_key == b"beacon1"
