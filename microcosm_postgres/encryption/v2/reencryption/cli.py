f8f4ff65c5f2dbf4c5b1452f7f09c0e16791cf9f
John Peterson <john.peterson@globality.com>
2023-10-19 15:08:34 +0100

    Fixing nits around cli setup

--- microcosm_postgres/encryption/v2/reencryption/cli.py
+++ microcosm_postgres/encryption/v2/reencryption/cli.py
@@ -1,18 +1,23 @@
 from argparse import ArgumentParser
 
+from microcosm_postgres.encryption.v2.reencryption.audit import run_audit
+
 
 class ReencryptionCli:
 
-    def __init__(self):
+    def __init__(self, reencrypt_fn, audit_fn=run_audit):
         self.parser = ArgumentParser(description="Reenecrption CLI")
         self.subparsers = self.parser.add_subparsers()
 
-    def add_reencrypt_command(self, fn):
+        self._add_reencrypt_command(reencrypt_fn)
+        self._add_audit_command(audit_fn)
+
+    def _add_reencrypt_command(self, fn):
         reencrypt_command_parser = self.subparsers.add_parser('reencrypt', help='Reencrypt some data')
         reencrypt_command_parser.add_argument("--client-id", help="The client id to reencrypt")
         reencrypt_command_parser.set_defaults(func=fn)
 
-    def add_audit_command(self, fn):
+    def _add_audit_command(self, fn):
         audit_command_parser = self.subparsers.add_parser('audit', help='Audit some data')
         audit_command_parser.set_defaults(func=fn)
 
--- microcosm_postgres/tests/encryption/v2/reencryption/test_cli.py
+++ microcosm_postgres/tests/encryption/v2/reencryption/test_cli.py
@@ -7,6 +7,18 @@ import pytest
 from microcosm_postgres.encryption.v2.reencryption.cli import ReencryptionCli
 
 
+def mock_reencrypt_fn(args):
+    assert args.client_id == "123"
+    return "Reencryption successful!"
+
+
+def mock_audit_fn(args):
+    return "Audit completed!"
+
+
+cli = ReencryptionCli(mock_reencrypt_fn, mock_audit_fn)
+
+
 # Helper function to check if an exception is raised
 def raises_help_text(func, expected_exit_code, *args, **kwargs):
     with pytest.raises(SystemExit) as e:
@@ -15,32 +27,16 @@ def raises_help_text(func, expected_exit_code, *args, **kwargs):
 
 
 def test_reencrypt_command_setup():
-    cli = ReencryptionCli()
-
-    def mock_fn(args):
-        assert args.client_id == "123"
-        return "Reencryption successful!"
-
-    cli.add_reencrypt_command(mock_fn)
-
     sys.argv = ["prog_name", "reencrypt", "--client-id", "123"]
     cli()
 
 
 def test_audit_command_setup():
-    cli = ReencryptionCli()
-
-    def mock_fn(args):
-        return "Audit completed!"
-
-    cli.add_audit_command(mock_fn)
-
     sys.argv = ["prog_name", "audit"]
     cli()
 
 
 def test_no_command_displays_help():
-    cli = ReencryptionCli()
     sys.argv = ["prog_name"]
 
     # Capture stdout
@@ -52,6 +48,5 @@ def test_no_command_displays_help():
 
 
 def test_invalid_command_displays_help():
-    cli = ReencryptionCli()
     sys.argv = ["prog_name", "invalid_command"]
     raises_help_text(cli, 2)  # We expect an exit code of 2 here
