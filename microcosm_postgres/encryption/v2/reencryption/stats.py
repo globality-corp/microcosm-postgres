b7b1d8fbb8f06f8e0ef22fb21b389b959eeddf82
John Peterson <john.peterson@globality.com>
2023-10-20 10:52:40 +0100

    adding statistics logging support + dry run

---
+++ microcosm_postgres/encryption/v2/reencryption/stats.py
@@ -0,0 +1,21 @@
+# TODO - move to microcosm_postgres
+from dataclasses import dataclass
+
+
+@dataclass
+class ReencryptionStatistic:
+    """
+    Class usage to track reencryption statistics
+    May be be used with the reencryption cli
+
+    """
+    model_name: str  # Name of the model being reencrypted
+    total_instances_found: int  # Total number of instances found for the model
+    instances_found_to_be_unencrypted: int  # Number of instances found to be unencrypted
+    instances_reencrypted: int  # Number of instances reencrypted
+
+    def log_stats(self):
+        print(f"Model: {self.model_name}")  # noqa: T201
+        print(f"- Total Instances Found: {self.total_instances_found}")  # noqa: T201
+        print(f"- Instances Found to be Unencrypted: {self.instances_found_to_be_unencrypted}")  # noqa: T201
+        print(f"- Instances Reencrypted: {self.instances_reencrypted}")  # noqa: T201
--- microcosm_postgres/encryption/v2/reencryption/utils.py
+++ microcosm_postgres/encryption/v2/reencryption/utils.py
@@ -51,17 +51,33 @@ class ModelWithEncryptionSearch(ModelWithEncryption):
     search_kwargs: None | dict = None
 
 
-def reencrypt_instance(session: Session, instance: Any, encryption_columns: list[str]) -> None:
+def reencrypt_instance(
+    session: Session, instance: Any, encryption_columns: list[str], dry_run: bool = False
+) -> tuple[bool, bool]:
     """
     Update the instance in a way in which the ORM is leveraged, so that writes leveraging
     encryption are used.
+
+    We return back a tuple of (found_to_be_unencrypted, change_commited)
+    - found_to_be_unencrypted: True if any of the columns of the instance are unencrypted
+    - change_commited: True if the instance was changed and committed to the database
     """
+    found_to_be_unencrypted = False
+    change_commited = False
     for column_name in encryption_columns:
-        # Make the encryption attributes dirty by setting their values.
-        # ie. instance.my_column = instance.my_column
-        setattr(instance, column_name, getattr(instance, column_name))
-        session.merge(instance)
-        session.commit()
+        # Check if the column is unencrypted
+        if getattr(instance, f"{column_name}_unencrypted") is not None:
+            found_to_be_unencrypted = True
+
+        if not dry_run:
+            # Make the encryption attributes dirty by setting their values.
+            # ie. instance.my_column = instance.my_column
+            setattr(instance, column_name, getattr(instance, column_name))
+            session.merge(instance)
+            session.commit()
+            change_commited = True
+
+    return found_to_be_unencrypted, change_commited
 
 
 def find_models_using_encryption(base_model: type = Model):
